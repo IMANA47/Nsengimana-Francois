111525d0858c38be0b2cc9a9a5c5bd83e700e48c
Francois Nsengimana <francknsengimana@gmail.com>
2026-03-06 13:45:53 +0100

    Testing the operation of components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+
+import { Camera } from 'lucide-vue-next';
 </script>
 
 <template>
   <div>
+    <Camera class="w-6 h-6 text-yellow-500" />
     <Button>Click me</Button>
   </div>
 </template>
